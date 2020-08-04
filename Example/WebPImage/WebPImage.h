87033db87e937562c25a7617d2f071c241e7ffce
SuperY <yuanlinyue@gmail.com>
2020-08-04 11:42:04 +0800

    Make framework for carthage.

---
+++ Example/WebPImage/WebPImage.h
@@ -0,0 +1,19 @@
+//
+//  WebPImage.h
+//  WebPImage
+//
+//  Created by Felix Yuan on 2020/7/8.
+//  Copyright © 2020 Mattt Thompson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebPImage.
+FOUNDATION_EXPORT double WebPImageVersionNumber;
+
+//! Project version string for WebPImage.
+FOUNDATION_EXPORT const unsigned char WebPImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebPImage/PublicHeader.h>
+
+
